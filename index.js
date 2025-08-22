bc3844e0039d3ccc18cbf31b86c63b4b36e8bc6c
tenshi t <cursors.owo.uwu@gmail.com>
2025-08-22 18:11:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -120,13 +120,13 @@ window.addEventListener("mouseup", () => {
     dragged.length = 0;
 });
 const titles = [
-    "mchccn",
-    "_chccn",
-    "m_hccn",
-    "mc_ccn",
-    "mch_cn",
-    "mchc_n",
-    "mchcc_",
+    "k-tten",
+    "_-tten",
+    "k_tten",
+    "k-_ten",
+    "k-t_en",
+    "k-tt_n",
+    "k-tte_",
 ];
 let i = 0;
 setInterval(() => (document.title = titles[(i = i >= titles.length - 1 ? 0 : i + 1)]), 1000);
